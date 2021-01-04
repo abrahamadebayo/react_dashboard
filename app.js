17349bcc934e50c794fb7229a0228b22abfbe591
Adebayo Quadri <adebayo.abraham@gmail.com>
2021-01-04 20:59:36 +0100

    Default with create-react-app
    Default installation using create-react-app

--- app.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-app.use(express.static('main'));
-app.use(function (req, res) {
-    res.sendFile(__dirname + '/main/index.html');
-});
-app.listen(8333, function () {
-    console.log('Read_APi is Using port 8333! With Express Js');
-});
